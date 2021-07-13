0e278a83964fcb100c5b02606ada6e4942f49f72
Bast <neubeli@web.de>
2021-07-13 19:05:55 +0200

    ﻿Size of canvas can change and slider functions different options black, blue, eraser and clear. You can decide which size the canvas has by using the slider. Problem that specifies amount of cells in a row doesnt show the last cell because the others are a bit too large. Solution: Take 0.01 px from every cell off. The problem with the declaration of div is kinda fixed. The declaration is in the loop, but now the loop is inside a function to create the grid.

--- main.js
+++ main.js
@@ -1,86 +1,130 @@
 // declare Variables
-let numberCellsInARow = 16;
+let numberCellsInARow;
 let numberCellsInCanvas;
 let loopIterationNumber;
 let screenwidth;
 let canvasWidth;
 let cellWidth;
-// let div;
+let backgroundColorForCells = "white";
+let divContainer;
 
 // Know if User is on Smartphone to make click instead of Hover Event
 var UserAgent = navigator.userAgent.toLowerCase();
 
 
-// Declare how many cells in a row from user (default 16*16)
+// querySelectors for Buttons - color and eraser
+const sketchBlack = document.querySelector('.Sketch');
+sketchBlack.addEventListener('click', () => {
+   backgroundColorForCells = "black";
+})
+
+const eraseCells = document.querySelector('.Eraser');
+eraseCells.addEventListener('click', () => {
+    backgroundColorForCells = "white";
+})
+
+const sketchBlue = document.querySelector('.Blue');
+sketchBlue.addEventListener('click', () => {
+    backgroundColorForCells = "cyan";
+})
 
 
+// Declare how many cells in a row from user (default 16)
+    const showNumberSlider = document.querySelector('.cellsValueDisplay');
+    const cellsSizeSlider = document.getElementById('cellsSizeSlider');
+    showNumberSlider.textContent = `${cellsSizeSlider.value}`
+    
+    cellsSizeSlider.addEventListener('change', ()=> {
+        showNumberSlider.textContent = `${cellsSizeSlider.value}`
+        numberCellsInARow = cellsSizeSlider.value;
+        removeGrid()
+        makeGrid()
+    })
 
-// How many Cells in Canvas
-numberCellsInCanvas = numberCellsInARow*numberCellsInARow;
-console.log(numberCellsInCanvas);
-
-
-// Create QuerySelector Body to append Elements
-    const body = document.querySelector('.body');
-
-    // Create Container that appends on body
-    const divContainer = document.createElement('div');
-
-    // Give created Elements a Classname to style in css
-    divContainer.classList.add('Container');
-
-    // Append the elements
-    body.appendChild(divContainer);
 
 
 // loop that creates the right amount of cells 
 // and appends to container 
-// and set width of canvas and width of cells 
 // and hover or click
+function makeGrid() {
+
+// Create QuerySelector Body to append Elements
+const body = document.querySelector('.body');
+
+// Create Container that appends on body
+divContainer = document.createElement('div');
+
+// Give created Elements a Classname to style in css
+divContainer.classList.add('Container');
+
+// Append the element
+body.appendChild(divContainer);
+
+
+// width of screen -> width of canvas
+if(screen.width >= 1024) {
+    divContainer.style.width = 800 + "px";
+    
+    canvasWidth = 800;
+    console.log(canvasWidth);
+}  
+else {
+    divContainer.style.width = 400 + "px"; 
+    canvasWidth = 400;
+    console.log(canvasWidth);
+
+    
+
+}
+
+numberCellsInARow = cellsSizeSlider.value;
+    // How many Cells in Canvas
+    numberCellsInCanvas = numberCellsInARow*numberCellsInARow;
+    console.log(numberCellsInCanvas);
+
 for(loopIterationNumber = 0; loopIterationNumber < numberCellsInCanvas; loopIterationNumber++) {
     // create divs and append on Container
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.classList.add('Div');
     divContainer.appendChild(div);
 
-    // width of screen -> width of canvas
-        if(screen.width >= 1024) {
-            divContainer.style.width = numberCellsInARow*2 + "rem";
-            divContainer.style.cssText = 'grid-template-columns: 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem; grid-template-rows: 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem 2rem;'
-            canvasWidth = 32;
-        }  
-        else {
-            divContainer.style.width = numberCellsInARow + "rem"; 
-            canvasWidth = 16;
-        }  
     
     // declare width of cells
-        cellWidth = canvasWidth / numberCellsInARow;
-        div.style.width = cellWidth + "rem";
-        div.style.height = cellWidth + "rem";
-        console.log(cellWidth);
+    cellWidth = canvasWidth / numberCellsInARow;
+    div.style.width = cellWidth - 0.01 + "px";
+    div.style.height = cellWidth - 0.01 + "px";
+    console.log(cellWidth);
 
+    
+        
     // Smartphone or PC -> click or hover
         // User-Agent: if smartphone: click
         if (UserAgent.search(/(iphone|ipod|opera mini|fennec|palm|blackberry|android|symbian|series60|webos|ipad|windows phone)/) > -1) {
             
             div.addEventListener('click', () => {
-                div.style.backgroundColor = "cyan";
+                div.style.backgroundColor = backgroundColorForCells;
             });
         }
         // else PC: hover
         else {
             div.addEventListener('mouseenter', () => {
-                div.style.backgroundColor = "cyan";
+                div.style.backgroundColor = backgroundColorForCells;
             });
         }
+    
 
-        // Clear Button
-const clearCanvas = document.querySelector(".Clear");
+    // Clear Button
+    const clearCanvas = document.querySelector(".Clear");
 
-clearCanvas.addEventListener('click', () => {
-    div.style.backgroundColor = "white";
-}); 
+    clearCanvas.addEventListener('click', () => {
+        div.style.backgroundColor = "white";
+    }); 
+
+}
 }
 
-
+function removeGrid() {
+    const Cells = document.getElementsByClassName('.div');
+    divContainer.remove(Cells);
+}
+makeGrid();
